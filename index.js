aba3dc2bcb52b4a097d81576ae475627fbe84187
Lunar | Neptr <straxxmusicteam@gmail.com>
2022-03-30 00:11:09 -0400

    start to optimalStation algorithm

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ gasify('LOCATION PLACEHOLDER', (error, response) => {
         let stations = []
 
         response.forEach((station) => {
+
+            // TRY MAKING API REQUEST TO GET DISTANCE HERE
+            // also clean up this funciton, combine into gasify function
+
             var injectData = {
                 station_name : station.name,
                 station_address: station.vicinity,
@@ -18,5 +22,11 @@ gasify('LOCATION PLACEHOLDER', (error, response) => {
         })
 
         console.log(stations)
+        const maxRating = stations.reduce((prev, current) => (prev.station_rating > current.station_rating) ? prev : current)
+
+        // const lowPLevel = stations.reduce((prev, current) => ((prev.price_level < current.price_level)) ? prev : current)
+
+        console.log(maxRating)
+        // console.log(lowPLevel)
     }
 })
