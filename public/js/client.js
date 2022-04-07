160dcd84768692fd660f457440d895810d0a4567
Lunar | Neptr <straxxmusicteam@gmail.com>
2022-04-07 00:47:12 -0400

    prints all stations to screen

--- public/js/client.js
+++ public/js/client.js
@@ -2,14 +2,32 @@ console.log('client js loaded')
 
 const endpointURL = ''
 const form = document.getElementById('input-form')
-const inputValue = document.querySelector('input').value
+const inputValue = document.querySelector('input')
 
-function runRequest (e) {
-    e.preventDefault();
-    var div = document.createElement('div');
-    div.innerHTML = "<h1>test</h1><p>test1123123</p>";
-    div.style.color = 'red';
-    document.getElementById('outerDiv').appendChild(div);
-}
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
 
-form.addEventListener('submit', runRequest)
+    const location = inputValue.value
+    
+    var outerDiv = document.getElementById('outerDiv');
+    outerDiv.innerHTML = ''
+    
+    fetch('http://localhost:3000/endpoint?location=' + location).then((response) => {
+    response.json().then((data) => {
+        if(data.error) {
+            var div = document.createElement('div');
+            div.innerHTML = data.error;
+            div.style.color = 'red';
+            document.getElementById('outerDiv').appendChild(div);
+        } else {
+
+            data.results.stations.forEach((station) => {
+                var div = document.createElement('div');
+                div.innerHTML = station.station_name + ' - ' +  station.station_address;
+                div.style.color = 'red';
+                document.getElementById('outerDiv').appendChild(div);
+            })
+         }
+    })
+})
+})
