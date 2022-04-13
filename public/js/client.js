11dd87a8b6552a466c4fc0f676d3bb28163112f5
Gabriel Oliver <straxxmusicteam@gmail.com>
2022-04-13 23:26:38 +0000

    working pass of app

--- public/js/client.js
+++ public/js/client.js
@@ -6,11 +6,12 @@ const inputValue = document.querySelector('input')
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
-
+            document.getElementById('accordionExample').innerHTML = '';
+    
     const location = inputValue.value
     
-    var outerDiv = document.getElementById('outerDiv');
-    outerDiv.innerHTML = ''
+    var accordionDiv = document.getElementsByClassName('accordion');
+    accordionDiv.innerHTML = ''
     
     fetch(endpointURL + location).then((response) => {
     response.json().then((data) => {
@@ -18,14 +19,38 @@ form.addEventListener('submit', (e) => {
             var div = document.createElement('div');
             div.innerHTML = data.error;
             div.style.color = 'red';
-            document.getElementById('outerDiv').appendChild(div);
+            document.getElementById('accordionExample').appendChild(div);
         } else {
-
+            var index = 0
             data.results.stations.forEach((station) => {
+                index = index + 1
+                
                 var div = document.createElement('div');
-                div.innerHTML = station.station_name + ' - ' +  station.station_address;
-                div.style.color = 'red';
-                document.getElementById('outerDiv').appendChild(div);
+                div.innerHTML = `
+<div class="accordion-item">								     <h2 class="accordion-header" id="heading` + index + `">
+        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse` + index + `" aria-expanded="false" aria-controls="collapse` + index + `">` + station.station_name + `
+	    </button>
+	</h2>
+	<div id="collapse` + index + `" class="accordion-collapse collapse" aria-labelledby="heading` + index + `" data-bs-parent="#accordionExample">
+	    <div class="accordion-body"> 
+            <p class="result-header">Station Address</p> 
+            <p class="result-address">` + station.station_address + `</p> 
+            <br>
+            <p class="result-header">Station Rating</p>
+            <p class="result-rating">` + station.station_rating + ` / 5 stars</p>
+            <br>
+            <p class="result-header">Station Distance</p>
+            <p class="result-distance">` + station.distance + ` miles</p>
+            <br>
+            <p class="result-header">Trip Duration</p>
+            <p class="result-duration">` + station.time + ` minutes</p>
+            <br>
+            <p class="result-header">Price Level</p>
+            <p class="result-plevel">` + station.price_level + `</p>
+        </div>
+	</div>
+</div>`
+                    document.getElementById('accordionExample').appendChild(div);
             })
          }
     })
