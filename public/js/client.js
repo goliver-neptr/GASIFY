9cb66d37ab0bcdc9bad773fc07aa9b7bca6ab218
Gabriel Oliver <straxxmusicteam@gmail.com>
2022-04-19 14:41:35 +0000

    center text displayed info on optimal stations

--- public/js/client.js
+++ public/js/client.js
@@ -68,7 +68,7 @@ form.addEventListener('submit', (e) => {
                     <hr>
                     <p class="stationHeader">Closest Station</p>
                     <hr>
-                    <button class="accordion-button optimal collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseLDIST" aria-expanded="false" aria-controls="flush-collapseLDIST"><span class="station_name">`+ data.results.optimalStations.lowDistance.station_name + ` on ` + data.results.optimalStations.lowDistance.station_street_name + `</span><span class="duration">` + data.results.optimalStations.lowDistance.distance + ` miles</span></button>
+                    <button class="accordion-button optimal collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseLDIST" aria-expanded="false" aria-controls="flush-collapseLDIST"><span class="station_name">`+ data.results.optimalStations.lowDistance.station_name + ` on ` + data.results.optimalStations.lowDistance.station_street_name + `</span><span class="duration text-center">` + data.results.optimalStations.lowDistance.distance + ` miles</span></button>
                 </h2>
                 <div id="flush-collapseLDIST" class="accordion-collapse collapse" aria-labelledby="flush-headingLDIST" data-bs-parent="#accordionFlushExample">
                     <div class="accordion-body">
@@ -97,7 +97,7 @@ form.addEventListener('submit', (e) => {
                     <hr>
                     <p class="stationHeader">Highest Rated Station</p>
                     <hr>
-                    <button class="accordion-button optimal collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseHRATE" aria-expanded="false" aria-controls="flush-collapseHRATE"><span class="station_name">`+ data.results.optimalStations.maxRating.station_name + ` on ` + data.results.optimalStations.maxRating.station_street_name + `</span><span class="duration">` + data.results.optimalStations.maxRating.station_rating + ` stars</span></button>
+                    <button class="accordion-button optimal collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseHRATE" aria-expanded="false" aria-controls="flush-collapseHRATE"><span class="station_name">`+ data.results.optimalStations.maxRating.station_name + ` on ` + data.results.optimalStations.maxRating.station_street_name + `</span><span class="duration text-center">` + data.results.optimalStations.maxRating.station_rating + ` stars</span></button>
                 </h2>
                 <div id="flush-collapseHRATE" class="accordion-collapse collapse" aria-labelledby="flush-headingHRATE" data-bs-parent="#accordionFlushExample">
                     <div class="accordion-body">
@@ -132,7 +132,7 @@ form.addEventListener('submit', (e) => {
                     div.innerHTML = `<div class="accordion accordion-flush" id="accordionFlushExample">
 <div class="accordion-item my-2">
     <h2 class="accordion-header" id="flush-heading` + index + `">
-        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse` + index + `" aria-expanded="false" aria-controls="flush-collapse` + index + `"><span class="station_name">` + station.station_name + ` on ` + station.station_street_name + `</span><span class="duration">` + station.time + ` min</span></button>
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse` + index + `" aria-expanded="false" aria-controls="flush-collapse` + index + `"><span class="station_name">` + station.station_name + ` on ` + station.station_street_name + `</span><span class="duration text-center">` + station.time + ` min</span></button>
     </h2>
     <div id="flush-collapse` + index + `" class="accordion-collapse collapse" aria-labelledby="flush-heading` + index + `" data-bs-parent="#accordionFlushExample">
         <div class="accordion-body">
